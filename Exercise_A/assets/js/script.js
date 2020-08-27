97e5d578b8fe822e9c1b37bc2a116d91c60f721e
hilman72 <hilman91112@gmail.com>
2020-08-27 21:42:28 +0800

    finally

---
+++ Exercise_A/assets/js/script.js
@@ -0,0 +1,31 @@
+//navbar change color
+
+$(window).scroll(function () {
+  $("nav").toggleClass("scrolled", $(this).scrollTop() > 200);
+});
+
+//////////////////////////////////////////////
+
+//navbar  text  change color and size
+
+$(document).ready(function () {
+  var scroll_pos = 0;
+  $(document).scroll(function () {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 200) {
+      $(".bar_text").css("color", "#f05f40");
+      $(".bar_text").css("font-size", "1rem");
+      $(".bar_item_text").css("color", "#000");
+    } else {
+      $(".bar_text").css("color", "rgba(255, 255, 255, 0.7)");
+      $(".bar_text").css("font-size", "1.5rem");
+      $(".bar_item_text").css("color", "rgba(255, 255, 255, 0.7)");
+    }
+  });
+});
+
+//////////////////////////////////////////////
+
+// scrollspy
+
+$("body").scrollspy({ target: "#navbarSupportedContent" });
