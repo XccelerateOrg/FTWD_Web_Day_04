0176c938ef1fb4439187bdca69e8d10f359a4f4b
wongw859 <wongw859@gmail.com>
2020-08-30 23:10:44 +0800

    draft

--- Exercise_A/assets/js/script.js
+++ Exercise_A/assets/js/script.js
@@ -1,1 +1,26 @@
 // Write your JavaScript script here.
+
+const nav = document.getElementsByTagName("nav")[0];
+
+
+window.addEventListener("scroll", function() {
+    // console.log(window.scrollY);
+    if (window.scrollY > 100) {
+        nav.classList.add("customNav");
+        $("#navLogo").css({ color: '#F05F40' });
+        $("#myList").css({ color: 'black' });
+        // console.log('testing')
+    } else {
+        nav.classList.remove("customNav");
+        $("#navLogo").css({ color: 'rgba(255, 255, 255, .7)' });
+    }
+});
+
+// smooth scroll //
+// library
+var scroll = new SmoothScroll('a[href*="#"]');
+
+// smooth scroll jquery
+// $('.btn').on('click', function(e) {
+//     console.log(this.hash);
+// })
