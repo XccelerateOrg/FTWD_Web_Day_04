57c0314ffad33581d3418a7295f7374c72f34760
nathan19952008@gmail.com <nathan19952008@gmail.com>
2021-01-14 11:18:13 +0800

    TODO: Font, Copywriting

--- Exercise_A/assets/js/script.js
+++ Exercise_A/assets/js/script.js
@@ -1,1 +1,28 @@
 // Write your JavaScript script here.
+$(document).ready(function () {
+  var toggleAffix = function (affixElement, scrollElement, wrapper) {
+    var height = affixElement.outerHeight(),
+      top = wrapper.offset().top;
+
+    if (scrollElement.scrollTop() >= top) {
+      wrapper.height(height);
+      affixElement.addClass("affix");
+    } else {
+      affixElement.removeClass("affix");
+      wrapper.height("auto");
+    }
+  };
+
+  $('[data-toggle="affix"]').each(function () {
+    var ele = $(this),
+      wrapper = $("<div></div>");
+
+    ele.before(wrapper);
+    $(window).on("scroll resize", function () {
+      toggleAffix(ele, $(this), wrapper);
+    });
+
+    // init
+    toggleAffix(ele, $(window), wrapper);
+  });
+});
