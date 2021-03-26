08cb3081c47867fefa2caac81b7da836d4edeaf9
cheungchikting <cheungchikting@gmail.com>
2021-03-26 14:47:26 +0800

    Completed

--- Exercise_A/assets/js/script.js
+++ Exercise_A/assets/js/script.js
@@ -1,1 +1,29 @@
-// Write your JavaScript script here.
+
+
+    let navbar = document.querySelector('#jcnav');
+    let navlogo = document.querySelector('#jclogo');
+    let navlink = document.querySelectorAll('#jclink');
+      window.onscroll = () => {
+      if (window.scrollY > 300) {
+        navbar.classList.add('nav-active');
+        navlogo.classList.add('newlogo');
+        for (var i = 0; i < navlink.length; i++) {
+            navlink[i].classList.add('newlink');
+        };
+      } else {
+        navbar.classList.remove('nav-active');
+        navlogo.classList.remove('newlogo');
+        for (var i = 0; i < navlink.length; i++) {
+            navlink[i].classList.remove('newlink');
+        };
+      }
+    };
+
+    // let navlogo = document.querySelector('#jclogo');
+    //   window.onscroll = () => {
+    //   if (window.scrollY > 300) {
+    //     navlogo.classList.add('newlogo');
+    //   } else {
+    //     navlogo.classList.remove('newlogo');
+    //   }
+    // };
