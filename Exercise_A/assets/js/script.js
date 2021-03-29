eefddc0d87e118c9f7ff27a2ee7ae076dcad7e4d
david Cheung <davidcheung888@hotmail.com>
2021-03-29 14:32:41 +0800

    done

--- Exercise_A/assets/js/script.js
+++ Exercise_A/assets/js/script.js
@@ -1,1 +1,9 @@
 // Write your JavaScript script here.
+// console.log("hello")
+
+// var number = 1
+// console.log(number);
+
+// console.log(document.getElementById('alter'))
+
+// document.getElementById('alter').innerHTML = number
