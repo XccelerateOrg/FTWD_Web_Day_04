b2a7c42befb38ba5f0752010d1813c2a03c1cf44
Kaho Pong <pongkaho@gmail.com>
2021-10-23 17:55:22 +0800

    Finished Exercise

--- Exercise_A/assets/js/script.js
+++ Exercise_A/assets/js/script.js
@@ -1,1 +1,34 @@
 // Write your JavaScript script here.
+var navItems = document.getElementsByClassName('nav-link');
+for (let i = 0; i < navItems.length; i++) {
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 100) {
+            navItems[i].classList.add('scrollNavItems',
+                'scrollNavItems:hover');
+        } else {
+            navItems[i].classList.remove('scrollNavItems')
+        }
+    })
+}
+
+var navLogo = document.getElementsByClassName('navbar-brand');
+for (let i = 0; i < navLogo.length; i++) {
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 100) {
+            navLogo[i].classList.add('scrollNavLogo', 'scrollNavLogo:hover');
+        } else {
+            navLogo[i].classList.remove('scrollNavLogo')
+        }
+    })
+}
+
+var navBar = document.querySelector('nav');
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 100) {
+        navBar.classList.add('scrollBar');
+    } else {
+        navBar.classList.remove('scrollBar')
+
+
+    }
+})
