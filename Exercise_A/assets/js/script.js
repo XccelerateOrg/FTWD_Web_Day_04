6e948ae5bdf0fd70a0d9d1ca091b35e1d950a665
lawfuhin@gmail.com <your_github_email>
2022-09-25 10:59:00 +0800

    finish

--- Exercise_A/assets/js/script.js
+++ Exercise_A/assets/js/script.js
@@ -1,1 +1,16 @@
-// Write your JavaScript script here.
+const navBar = document.getElementsByClassName("navbar-expand-lg")[0];
+const navItem = document.getElementsByClassName("navbar-nav");
+
+console.log(document.getElementsByClassName("navbar"));
+console.log(document.getElementsByClassName("nav-link"));
+
+window.addEventListener("scroll", function () {
+  console.log(window.scrollY);
+
+  if (window.scrollY <= 100) {
+      navBar.classList.remove("bg-light");
+      
+  } else if (window.scrollY > 100) {
+      navBar.classList.add("bg-light");
+  }
+});
